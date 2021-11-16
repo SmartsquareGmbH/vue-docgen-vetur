cbbac1f6a05870feaeb8dd520b71e1c9149d8211
Ruben Gees <rubengees7@googlemail.com>
2021-11-16 23:57:59 +0100

    Add tests for the cli

---
+++ tests/index.spec.ts
@@ -0,0 +1,39 @@
+import path from "path"
+import fs from "fs/promises"
+import execSh from "exec-sh"
+import tmp, { DirectoryResult } from "tmp-promise"
+
+describe("index.js", () => {
+  let tmpDir!: DirectoryResult
+
+  beforeEach(async () => {
+    tmpDir = await tmp.dir()
+  })
+
+  afterEach(async () => {
+    await tmpDir.cleanup
+  })
+
+  test("print version", async () => {
+    const result = await execSh.promise(["ts-node src/index.ts -v"], true)
+
+    expect(result.stderr).toEqual("")
+    expect(result.stdout).toContain(await getVersion())
+  })
+
+  test("run", async () => {
+    const result = await execSh.promise([`ts-node src/index.ts -o ${tmpDir.path} tests/__data__/*`], true)
+
+    expect(result.stderr).toEqual("")
+
+    // Check if files exist.
+    await fs.access(path.join(tmpDir.path, "tags.json"))
+    await fs.access(path.join(tmpDir.path, "attributes.json"))
+  })
+})
+
+async function getVersion(): Promise<string> {
+  const packageJson = await fs.readFile("package.json")
+
+  return JSON.parse(packageJson.toString()).version
+}
